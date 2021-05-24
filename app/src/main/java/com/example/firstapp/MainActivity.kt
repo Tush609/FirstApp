c971ce7448e7b9c1da16b6a88fb4e6b1fd893c22
Tush609 <83871164+Tush609@users.noreply.github.com>
2021-05-24 19:43:40 +0530

    Portriat for Regular Size

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.firstapp
 
+import android.graphics.BitmapFactory
+import android.media.tv.TvContract
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.core.graphics.drawable.RoundedBitmapDrawable
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import org.xmlpull.v1.XmlPullParser
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        rounded.cornerRadius = 15f
+
+
     }
 }
